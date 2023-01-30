8d5d5d44dfd7ed775a25075092d2acc26d992e07
Son Do <dominhson580@gmail.com>
2023-01-30 23:14:22 +0700

    Set up Redux Devtools extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducer";
 
-const store = createStore(rootReducer);
+const composeEnhancers = composeWithDevTools();
+
+const store = createStore(rootReducer, composeEnhancers);
 
 export default store;
