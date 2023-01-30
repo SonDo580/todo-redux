7dc3241d0ba279859f22e3994b8cc8bd9e0d2d55
Son Do <dominhson580@gmail.com>
2023-01-30 16:33:52 +0700

    Define TodoItem

--- src/components/Todo/TodoItem/index.js
+++ src/components/Todo/TodoItem/index.js
@@ -1,5 +1,22 @@
-function TodoItem() {
-  return <div>TodoItem</div>;
+import { Checkbox, Row, Tag } from "antd";
+
+const priorityColorMapping = {
+  High: "red",
+  Medium: "blue",
+  Low: "gray",
+};
+
+function TodoItem(props) {
+  const {
+    todo: { title, priority },
+  } = props;
+
+  return (
+    <Row justify="space-between" style={{ marginBottom: "5px" }}>
+      <Checkbox>{title}</Checkbox>
+      <Tag color={priorityColorMapping[priority]}>{priority}</Tag>
+    </Row>
+  );
 }
 
 export default TodoItem;
