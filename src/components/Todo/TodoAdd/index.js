3d65f6d98dc4ca28d277d643f0da6e2c8ed9dddf
Son Do <dominhson580@gmail.com>
2023-01-30 22:08:33 +0700

    Handle clicking add-todo button

--- src/components/Todo/TodoAdd/index.js
+++ src/components/Todo/TodoAdd/index.js
@@ -1,9 +1,20 @@
 import { Button, Input, Select, Tag } from "antd";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../../../redux/actions";
 
 function TodoAdd() {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(
+      addTodo({ id: 1000, title: "Test", priority: "High", completed: false })
+    );
+  };
+
   return (
     <Input.Group style={{ display: "flex" }} compact>
       <Input placeholder="new task" />
+
       <Select defaultValue="Medium">
         <Select.Option value="High" label="High">
           <Tag color="red">High</Tag>
@@ -15,7 +26,10 @@ function TodoAdd() {
           <Tag color="gray">Low</Tag>
         </Select.Option>
       </Select>
-      <Button type="primary">Add</Button>
+
+      <Button type="primary" onClick={handleClick}>
+        Add
+      </Button>
     </Input.Group>
   );
 }
