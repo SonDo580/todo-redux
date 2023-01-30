9b14c0d254efd94a6e6fea7b250fb6aa29394a2b
Son Do <dominhson580@gmail.com>
2023-01-30 15:35:44 +0700

    Add label

--- src/components/Filters/SearchField/index.js
+++ src/components/Filters/SearchField/index.js
@@ -1,8 +1,17 @@
-import { Input } from "antd";
+import { Input, Typography } from "antd";
 const { Search } = Input;
 
 function SearchField() {
-  return <Search placeholder="input search text" />;
+  return (
+    <div>
+      <Typography.Paragraph
+        style={{ fontWeight: "bold", marginBottom: 3, marginTop: 10 }}
+      >
+        Search
+      </Typography.Paragraph>
+      <Search placeholder="input search text" />
+    </div>
+  );
 }
 
 export default SearchField;
