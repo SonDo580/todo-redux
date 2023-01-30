142daac03ccd81b8c3fb8fe40af5e8cdc17da681
Son Do <dominhson580@gmail.com>
2023-01-30 22:59:28 +0700

    Get todoList from redux store

--- src/components/Todo/TodoList/index.js
+++ src/components/Todo/TodoList/index.js
@@ -1,15 +1,10 @@
+import { useSelector } from "react-redux";
 import TodoAdd from "../TodoAdd";
 import TodoItem from "../TodoItem";
 
-const todoList = [
-  { id: 1, task: "Learn React", priority: "High" },
-  { id: 2, task: "Learn Express", priority: "High" },
-  { id: 3, task: "Learn Redux", priority: "Medium" },
-  { id: 4, task: "Learn Firebase", priority: "Medium" },
-  { id: 5, task: "Learn Networking", priority: "Low" },
-];
-
 function TodoList() {
+  const todoList = useSelector((state) => state.todos);
+
   return (
     <div style={{ height: "50%" }}>
       <div style={{ height: "80%", overflow: "auto" }}>
