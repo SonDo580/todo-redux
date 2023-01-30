5190a42d9a9ae1fdfb27d4dd00b100a035c64f60
Son Do <dominhson580@gmail.com>
2023-01-30 16:50:57 +0700

    Define add-task input group

--- src/components/Todo/TodoAdd/index.js
+++ src/components/Todo/TodoAdd/index.js
@@ -1,5 +1,23 @@
+import { Button, Input, Select, Tag } from "antd";
+
 function TodoAdd() {
-  return <div>TodoAdd</div>;
+  return (
+    <Input.Group style={{ display: "flex" }} compact>
+      <Input placeholder="new task" />
+      <Select defaultValue="Medium">
+        <Select.Option value="High" label="High">
+          <Tag color="red">High</Tag>
+        </Select.Option>
+        <Select.Option value="Medium" label="Medium">
+          <Tag color="blue">Medium</Tag>
+        </Select.Option>
+        <Select.Option value="Low" label="Low">
+          <Tag color="gray">Low</Tag>
+        </Select.Option>
+      </Select>
+      <Button type="primary">Add</Button>
+    </Input.Group>
+  );
 }
 
 export default TodoAdd;
