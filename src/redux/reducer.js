02ac80122d20d2afc2e01a50c41bbcf550a3bde7
Son Do <dominhson580@gmail.com>
2023-01-30 21:46:30 +0700

    Add initial state

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,16 @@
 const initialState = {
-  filters: {},
-  todoList: [],
+  filters: {
+    search: "",
+    status: "All",
+    priority: [],
+  },
+  todoList: [
+    { id: 1, title: "Learn React", priority: "High", completed: true },
+    { id: 2, title: "Learn Express", priority: "High", completed: false },
+    { id: 3, title: "Learn Redux", priority: "Medium", completed: true },
+    { id: 4, title: "Learn Firebase", priority: "Medium", completed: false },
+    { id: 5, title: "Learn Networking", priority: "Low", completed: false },
+  ],
 };
 
 const rootReducer = (state = initialState, action) => {
