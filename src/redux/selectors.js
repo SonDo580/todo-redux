79b926351c2c474fd59928f1ffa4c54f50ae52f2
Son Do <dominhson580@gmail.com>
2023-01-31 11:49:22 +0700

    Create todosRemainingSelector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,8 +1,14 @@
-export const todoListSelector = (state) =>
-  state.todos.filter((todo) => {
-    const searchText = searchTextSelector(state);
+import { createSelector } from "reselect";
 
-    return todo.task.toLowerCase().includes(searchText);
-  });
+const todoListSelector = (state) => state.todos;
+const searchTextSelector = (state) => state.filters.search;
 
-export const searchTextSelector = (state) => state.filters.search;
+export const todosRemainingSelector = createSelector(
+  todoListSelector,
+  searchTextSelector,
+  (todoList, searchText) => {
+    return todoList.filter((todo) =>
+      todo.task.toLowerCase().includes(searchText)
+    );
+  }
+);
