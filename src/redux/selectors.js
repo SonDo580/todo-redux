456a42a80009c9e13012f5109077baa40edf1e44
Son Do <dominhson580@gmail.com>
2023-01-31 14:12:54 +0700

    Filter by status

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,13 +2,23 @@ import { createSelector } from "reselect";
 
 const todoListSelector = (state) => state.todos;
 const searchTextSelector = (state) => state.filters.search;
+const statusFilterSelector = (state) => state.filters.status;
 
 export const todosRemainSelector = createSelector(
   todoListSelector,
   searchTextSelector,
-  (todoList, searchText) => {
-    return todoList.filter((todo) =>
-      todo.task.toLowerCase().includes(searchText)
-    );
+  statusFilterSelector,
+  (todoList, searchText, statusFilter) => {
+    return todoList
+      .filter((todo) => todo.task.toLowerCase().includes(searchText))
+      .filter((todo) => {
+        if (statusFilter === "All") {
+          return true;
+        } else if (statusFilter === "Completed") {
+          return todo.completed;
+        } else {
+          return !todo.completed;
+        }
+      });
   }
 );
