e6a32f5bf569d07f1d8ba1cc93eaa48fb26156dd
Son Do <dominhson580@gmail.com>
2023-01-31 16:39:41 +0700

    Use createSelector from redux toolkit

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,4 @@
-import { createSelector } from "reselect";
+import { createSelector } from "@reduxjs/toolkit";
 
 const todoListSelector = (state) => state.todos;
 const searchTextSelector = (state) => state.filters.search;
