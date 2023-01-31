e36f8a93658a382d81f23a5ce5bb6febc96ead70
Son Do <dominhson580@gmail.com>
2023-01-31 11:44:21 +0700

    Extract variable searchText

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,6 +1,8 @@
 export const todoListSelector = (state) =>
-  state.todos.filter((todo) =>
-    todo.task.toLowerCase().includes(searchTextSelector())
-  );
+  state.todos.filter((todo) => {
+    const searchText = searchTextSelector(state);
+
+    return todo.task.toLowerCase().includes(searchText);
+  });
 
 export const searchTextSelector = (state) => state.filters.search;
