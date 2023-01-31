b328237457db552b66c97e502b9c19d2922439c3
Son Do <dominhson580@gmail.com>
2023-01-31 11:42:33 +0700

    Use todoListSelector

--- src/components/Todo/TodoList/index.js
+++ src/components/Todo/TodoList/index.js
@@ -1,19 +1,15 @@
 import { useSelector } from "react-redux";
+import { todoListSelector } from "../../../redux/selectors";
 import TodoAdd from "../TodoAdd";
 import TodoItem from "../TodoItem";
 
 function TodoList() {
-  const todoList = useSelector((state) => state.todos);
-  const searchText = useSelector((state) => state.filters.search);
-
-  const todoRemain = todoList.filter((todo) =>
-    todo.task.toLowerCase().includes(searchText)
-  );
+  const todoList = useSelector(todoListSelector);
 
   return (
     <div style={{ height: "50%" }}>
       <div style={{ height: "80%", overflow: "auto" }}>
-        {todoRemain.map((todo) => (
+        {todoList.map((todo) => (
           <TodoItem key={todo.id} todo={todo} />
         ))}
       </div>
