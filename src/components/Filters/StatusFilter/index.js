f49f2918548551236d7c09d10e29a78b9594837a
Son Do <dominhson580@gmail.com>
2023-01-31 14:04:26 +0700

    Add state 'status'

--- src/components/Filters/StatusFilter/index.js
+++ src/components/Filters/StatusFilter/index.js
@@ -1,6 +1,13 @@
 import { Radio, Typography } from "antd";
+import { useState } from "react";
 
 function StatusFilter() {
+  const [status, setStatus] = useState("All");
+
+  const handleStatusChange = (event) => {
+    setStatus(event.target.value);
+  };
+
   return (
     <div>
       <Typography.Paragraph
@@ -9,7 +16,7 @@ function StatusFilter() {
         Filter By Status
       </Typography.Paragraph>
 
-      <Radio.Group>
+      <Radio.Group value={status} onChange={handleStatusChange}>
         <Radio value="All">All</Radio>
         <Radio value="Completed">Completed</Radio>
         <Radio value="Todo">To do</Radio>
