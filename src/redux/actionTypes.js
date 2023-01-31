9fd12cf7d7d2c32361582dd10700b9d0d23529af
Son Do <dominhson580@gmail.com>
2023-01-31 11:05:42 +0700

    Create action search todo

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,1 +1,2 @@
 export const ADD_TODO = "todos/addTodo";
+export const SEARCH_TODO = "filters/search";
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,11 @@
-import { ADD_TODO } from "./actionTypes";
+import { ADD_TODO, SEARCH_TODO } from "./actionTypes";
 
 export const addTodo = (todo) => ({
   type: ADD_TODO,
   payload: todo,
+});
+
+export const searchTodo = (searchText) => ({
+  type: SEARCH_TODO,
+  payload: searchText,
 });
