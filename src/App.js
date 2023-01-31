2ea4bec8cd2544031a3ca99d87ee01f9451ee599
Son Do <dominhson580@gmail.com>
2023-01-31 21:33:46 +0700

    Set up (mock) server for the app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 import { Divider, Typography } from "antd";
 import Filters from "./components/Filters";
 import TodoList from "./components/Todo/TodoList";
-
+import { setupServer } from "./fakeAPIs";
 const { Title } = Typography;
+
+setupServer();
 
 function App() {
   return (
