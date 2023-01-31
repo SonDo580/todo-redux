a46fe442a52cb573eddc0aa9435251c66ba3da81
Son Do <dominhson580@gmail.com>
2023-01-31 16:17:09 +0700

    Create filterSlice with createSlice

--- src/components/Filters/filterSlice.js
+++ src/components/Filters/filterSlice.js
@@ -1,38 +1,25 @@
-import {
-  PRIORITY_FILTER,
-  SEARCH_TODO,
-  STATUS_FILTER,
-} from "../../redux/actionTypes";
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  search: "",
-  status: "All",
-  priority: [],
-};
+const filterSlice = createSlice({
+  name: "filters",
+  initialState: {
+    search: "",
+    status: "All",
+    priority: [],
+  },
+  reducers: {
+    searchTodo: (state, action) => {
+      state.search = action.payload;
+    },
+    filterByStatus: (state, action) => {
+      state.status = action.payload;
+    },
+    filterByPriority: (state, action) => {
+      state.priority = action.payload;
+    },
+  },
+});
 
-const filterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case SEARCH_TODO:
-      return {
-        ...state,
-        search: action.payload.toLowerCase(),
-      };
-
-    case STATUS_FILTER:
-      return {
-        ...state,
-        status: action.payload,
-      };
-
-    case PRIORITY_FILTER:
-      return {
-        ...state,
-        priority: action.payload,
-      };
-
-    default:
-      return state;
-  }
-};
-
-export default filterReducer;
+const { reducer, actions } = filterSlice;
+export const { searchTodo, filterByStatus, filterByPriority } = actions;
+export default reducer;
