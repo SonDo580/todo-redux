8181c2b9b24bca2360665f9915b5b8a0c99ad6da
Son Do <dominhson580@gmail.com>
2023-02-01 11:04:00 +0700

    Dispatch fetchTodos thunk

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Divider, Typography } from "antd";
 import Filters from "./components/Filters";
 import TodoList from "./components/Todo/TodoList";
 import { setupServer } from "./fakeAPIs";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchTodos } from "./components/Todo/todoSlice";
 const { Title } = Typography;
 
 // if (process.env.NODE_ENV === 'development') {
@@ -10,6 +13,12 @@ setupServer();
 // }
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, []);
+
   return (
     <div className="App">
       <Title style={{ textAlign: "center" }}>TODO APP with REDUX</Title>
