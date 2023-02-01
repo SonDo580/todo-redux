f9c9b2d0903f8b269b1e5972a4b496098e83dde9
Son Do <dominhson580@gmail.com>
2023-02-01 11:57:19 +0700

    Add initial todoList

--- src/fakeAPIs/index.js
+++ src/fakeAPIs/index.js
@@ -1,9 +1,21 @@
 import { createServer, Model } from "miragejs";
 
+const initialTodos = [
+  { id: 1, task: "Learn React", priority: "High", completed: true },
+  { id: 2, task: "Learn Express", priority: "High", completed: false },
+  { id: 3, task: "Learn Redux", priority: "Medium", completed: true },
+  { id: 4, task: "Learn Firebase", priority: "Medium", completed: false },
+  { id: 5, task: "Learn Networking", priority: "Low", completed: false },
+];
+
 export function setupServer() {
   createServer({
     models: {
       todo: Model,
+    },
+
+    seeds(server) {
+      initialTodos.forEach((todo) => server.create("todo", todo));
     },
 
     routes() {
